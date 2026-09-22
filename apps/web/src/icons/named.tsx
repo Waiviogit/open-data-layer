9e3095264f2f65efb58dfb285462c639a83c253d
fesmofet <borees2@gmail.com>
2026-09-22 15:07:16 +0300

    feat: Add new icons including Briefcase, Building, Chef Hat, Cup Soda, Hand, Hand Helping, Landmark, List, Map, Package, Puzzle, Rss, Share, Sparkles, Store, Utensils, and Utensils Crossed to the icon library, enhancing visual options for UI components

--- apps/web/src/icons/named.tsx
+++ apps/web/src/icons/named.tsx
@@ -15,11 +15,14 @@ export const BellIcon = createNamedIcon('bell');
 export const BoldIcon = createNamedIcon('bold');
 export const BookIcon = createNamedIcon('book');
 export const BookOpenIcon = createNamedIcon('book-open');
+export const BriefcaseIcon = createNamedIcon('briefcase');
+export const BuildingIcon = createNamedIcon('building');
 export const BrandFacebookIcon = createNamedIcon('brand-facebook');
 export const BrandXIcon = createNamedIcon('brand-x');
 export const CalendarIcon = createNamedIcon('calendar');
 export const CheckIcon = createNamedIcon('check');
 export const CheckCircleIcon = createNamedIcon('check-circle');
+export const ChefHatIcon = createNamedIcon('chef-hat');
 export const CircleStarIcon = createNamedIcon('circle-star');
 export const ChevronDownIcon = createNamedIcon('chevron-down');
 export const ChevronLeftIcon = createNamedIcon('chevron-left');
@@ -29,6 +32,7 @@ export const CloseIcon = createNamedIcon('close');
 export const CodeIcon = createNamedIcon('code');
 export const CommentIcon = createNamedIcon('comment');
 export const CopyIcon = createNamedIcon('copy');
+export const CupSodaIcon = createNamedIcon('cup-soda');
 export const DimensionsIcon = createNamedIcon('dimensions');
 export const DollarIcon = createNamedIcon('dollar');
 export const EmojiIcon = createNamedIcon('emoji');
@@ -38,6 +42,8 @@ export const FileTextIcon = createNamedIcon('file-text');
 export const FilterIcon = createNamedIcon('filter');
 export const FlagIcon = createNamedIcon('flag');
 export const GlobeIcon = createNamedIcon('globe');
+export const HandIcon = createNamedIcon('hand');
+export const HandHelpingIcon = createNamedIcon('hand-helping');
 export const HashIcon = createNamedIcon('hash');
 export const HbdSavingsShieldIcon = createNamedIcon('hbd-savings-shield');
 export const HeartIcon = createNamedIcon('heart');
@@ -45,36 +51,45 @@ export const HiveSavingsShieldIcon = createNamedIcon('hive-savings-shield');
 export const ImageIcon = createNamedIcon('image');
 export const InfoIcon = createNamedIcon('info');
 export const ItalicIcon = createNamedIcon('italic');
+export const LandmarkIcon = createNamedIcon('landmark');
 export const LayoutGridIcon = createNamedIcon('layout-grid');
 export const LinkIcon = createNamedIcon('link');
+export const ListIcon = createNamedIcon('list');
 export const LocateIcon = createNamedIcon('locate');
 export const LockIcon = createNamedIcon('lock');
 export const LockOpenIcon = createNamedIcon('lock-open');
 export const MailIcon = createNamedIcon('mail');
+export const MapIcon = createNamedIcon('map');
 export const MapPinIcon = createNamedIcon('map-pin');
 export const MaximizeIcon = createNamedIcon('maximize');
 export const MinimizeIcon = createNamedIcon('minimize');
 export const MinusIcon = createNamedIcon('minus');
 export const MoreHorizontalIcon = createNamedIcon('more-horizontal');
 export const MuteIcon = createNamedIcon('mute');
+export const PackageIcon = createNamedIcon('package');
 export const PencilIcon = createNamedIcon('pencil');
 export const PenLineIcon = createNamedIcon('pen-line');
 export const PhoneIcon = createNamedIcon('phone');
 export const PinIcon = createNamedIcon('pin');
 export const PlayIcon = createNamedIcon('play');
 export const PlusIcon = createNamedIcon('plus');
+export const PuzzleIcon = createNamedIcon('puzzle');
 export const QrCodeIcon = createNamedIcon('qr-code');
 export const ReblogIcon = createNamedIcon('reblog');
 export const ReplyIcon = createNamedIcon('reply');
 export const RewardFlashlightIcon = createNamedIcon('reward-flashlight');
+export const RssIcon = createNamedIcon('rss');
 export const RulerDimensionLineIcon = createNamedIcon('ruler-dimension-line');
 export const SearchIcon = createNamedIcon('search');
 export const SendIcon = createNamedIcon('send');
 export const SendHorizontalIcon = createNamedIcon('send-horizontal');
+export const ShareIcon = createNamedIcon('share');
 export const ShoppingBagIcon = createNamedIcon('shopping-bag');
 export const ShoppingCartIcon = createNamedIcon('shopping-cart');
 export const SmartphoneIcon = createNamedIcon('smartphone');
+export const SparklesIcon = createNamedIcon('sparkles');
 export const StarIcon = createNamedIcon('star');
+export const StoreIcon = createNamedIcon('store');
 export const TableIcon = createNamedIcon('table');
 export const ThumbDownIcon = createNamedIcon('thumb-down');
 export const ThumbUpIcon = createNamedIcon('thumb-up');
@@ -84,6 +99,8 @@ export const UserIcon = createNamedIcon('user');
 export const UserMinusIcon = createNamedIcon('user-minus');
 export const UserPlusIcon = createNamedIcon('user-plus');
 export const UsersIcon = createNamedIcon('users');
+export const UtensilsIcon = createNamedIcon('utensils');
+export const UtensilsCrossedIcon = createNamedIcon('utensils-crossed');
 export const VideoIcon = createNamedIcon('video');
 export const WalletIcon = createNamedIcon('wallet');
 export const WalletPowerLightningIcon = createNamedIcon('wallet-power-lightning');
@@ -101,11 +118,14 @@ export const NAMED_ICON_BY_NAME = {
   bold: BoldIcon,
   book: BookIcon,
   'book-open': BookOpenIcon,
+  briefcase: BriefcaseIcon,
+  building: BuildingIcon,
   'brand-facebook': BrandFacebookIcon,
   'brand-x': BrandXIcon,
   calendar: CalendarIcon,
   check: CheckIcon,
   'check-circle': CheckCircleIcon,
+  'chef-hat': ChefHatIcon,
   'circle-star': CircleStarIcon,
   'chevron-down': ChevronDownIcon,
   'chevron-left': ChevronLeftIcon,
@@ -115,6 +135,7 @@ export const NAMED_ICON_BY_NAME = {
   code: CodeIcon,
   comment: CommentIcon,
   copy: CopyIcon,
+  'cup-soda': CupSodaIcon,
   dimensions: DimensionsIcon,
   dollar: DollarIcon,
   emoji: EmojiIcon,
@@ -124,6 +145,8 @@ export const NAMED_ICON_BY_NAME = {
   filter: FilterIcon,
   flag: FlagIcon,
   globe: GlobeIcon,
+  hand: HandIcon,
+  'hand-helping': HandHelpingIcon,
   hash: HashIcon,
   'hbd-savings-shield': HbdSavingsShieldIcon,
   heart: HeartIcon,
@@ -131,36 +154,45 @@ export const NAMED_ICON_BY_NAME = {
   image: ImageIcon,
   info: InfoIcon,
   italic: ItalicIcon,
+  landmark: LandmarkIcon,
   'layout-grid': LayoutGridIcon,
   link: LinkIcon,
+  list: ListIcon,
   locate: LocateIcon,
   lock: LockIcon,
   'lock-open': LockOpenIcon,
   mail: MailIcon,
+  map: MapIcon,
   'map-pin': MapPinIcon,
   maximize: MaximizeIcon,
   minimize: MinimizeIcon,
   minus: MinusIcon,
   'more-horizontal': MoreHorizontalIcon,
   mute: MuteIcon,
+  package: PackageIcon,
   pencil: PencilIcon,
   'pen-line': PenLineIcon,
   phone: PhoneIcon,
   pin: PinIcon,
   play: PlayIcon,
   plus: PlusIcon,
+  puzzle: PuzzleIcon,
   'qr-code': QrCodeIcon,
   reblog: ReblogIcon,
   reply: ReplyIcon,
   'reward-flashlight': RewardFlashlightIcon,
+  rss: RssIcon,
   'ruler-dimension-line': RulerDimensionLineIcon,
   search: SearchIcon,
   send: SendIcon,
   'send-horizontal': SendHorizontalIcon,
+  share: ShareIcon,
   'shopping-bag': ShoppingBagIcon,
   'shopping-cart': ShoppingCartIcon,
   smartphone: SmartphoneIcon,
+  sparkles: SparklesIcon,
   star: StarIcon,
+  store: StoreIcon,
   table: TableIcon,
   'thumb-down': ThumbDownIcon,
   'thumb-up': ThumbUpIcon,
@@ -170,6 +202,8 @@ export const NAMED_ICON_BY_NAME = {
   'user-minus': UserMinusIcon,
   'user-plus': UserPlusIcon,
   users: UsersIcon,
+  utensils: UtensilsIcon,
+  'utensils-crossed': UtensilsCrossedIcon,
   video: VideoIcon,
   wallet: WalletIcon,
   'wallet-power-lightning': WalletPowerLightningIcon,
